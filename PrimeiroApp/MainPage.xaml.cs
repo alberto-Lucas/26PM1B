10b643ea4685e589f4467f837da3524acea3e5ce
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2026-03-09 22:05:52 -0300

    PrimeiroApp
    Aula dia 09/03/2026

---
+++ PrimeiroApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace PrimeiroApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCliqueAqui_Clicked(object sender, EventArgs e)
+        {
+            //Utilizar DisplayAlert
+            //que é o similar do MessageBox
+
+            //Titulo
+            //Texto
+            //Botoes
+
+            DisplayAlert(
+                "Atenção!", 
+                "Texto digitado: " + txtExemplo.Text,
+                "Ok");
+        }
+    }
+}
