61283953c37a120690f806a712447ba460750156
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2026-05-18 22:02:53 -0300

    AppMVC

--- AppMVC/Views/pgVisualizarPessoaView.xaml.cs
+++ AppMVC/Views/pgVisualizarPessoaView.xaml.cs
@@ -1,29 +1,26 @@
+using AppMVC.Models;
+
 namespace AppMVC.Views;
 
 public partial class pgVisualizarPessoaView : ContentPage
 {
-	public pgVisualizarPessoaView()
+	//Adiciona no método contrstutor o recebimento
+	//do objeto via parametro
+	//Para isso precisamos importar a camada Models
+	//using NomeProjeto.Models;
+	public pgVisualizarPessoaView(Pessoa pessoa)
 	{
 		InitializeComponent();
-	}
+
+		//Mapear a tela com base no objeto
+		lblId.Text = pessoa.Id.ToString();
+		lblNome.Text = pessoa.Nome;
+		lblCPF.Text = pessoa.CPF;
+		imgCadastro.Source = pessoa.DirImagem;
+    }
 
     private void btnVoltar_Clicked(object sender, EventArgs e)
     {
-
-    }
-
-    private void btnAddImagem_Clicked(object sender, EventArgs e)
-    {
-
-    }
-
-    private void btnRemoverImagem_Clicked(object sender, EventArgs e)
-    {
-
-    }
-
-    private void btnSalvar_Clicked(object sender, EventArgs e)
-    {
-
+		Application.Current.MainPage.Navigation.PopAsync();
     }
 }
